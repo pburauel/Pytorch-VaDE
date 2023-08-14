25c0d05ff4c3104adc6dd8f087fb10a65956b5ba
pburauel <61065381+pburauel@users.noreply.github.com>
2023-08-14 15:18:24 -0700

    works

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,8 @@ import torch
 from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader, TensorDataset
 
+from sklearn.decomposition import PCA
+
 
 def get_mnist(data_dir='./data/mnist/',batch_size=128):
     train = MNIST(root=data_dir, train=True, download=True)
@@ -9,6 +11,10 @@ def get_mnist(data_dir='./data/mnist/',batch_size=128):
 
     x = torch.cat([train.data.float().view(-1,784)/255., test.data.float().view(-1,784)/255.], 0)
     y = torch.cat([train.targets, test.targets], 0)
+
+
+    pca = PCA(n_components=784)
+    x2 = torch.from_numpy(pca.fit_transform(x))
 
     dataset = dict()
     dataset['x'] = x
